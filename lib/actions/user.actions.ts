4d86fd89e5695ff202d570b4bd9f1b951f3a74a7
AndriiTsokur <tsokur@gmail.com>
2025-02-07 11:19:45 +0200

    feat: Add error formatting utility for improved user feedback on registration

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { hashSync } from 'bcrypt-ts-edge';
 import { signInFormSchema, signUpFormSchema } from '@/lib/validators';
 import { signIn, signOut } from '@/auth';
 import { prisma } from '@/db/prisma';
+import { formatError } from '@/lib/utils';
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -66,6 +67,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
 			throw error;
 		}
 
-		return { success: false, message: 'User was not registered' };
+		return { success: false, message: formatError(error) };
 	}
 }
